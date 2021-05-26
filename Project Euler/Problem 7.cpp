09bb1a50de42f78e613b0749af1c4eb77f6ae210
ddk992001 <63284880+ddk992001@users.noreply.github.com>
2021-05-26 22:52:44 +0700

    add p7

---
+++ Project Euler/Problem 7.cpp
@@ -0,0 +1,27 @@
+// 28 - 12 - 2001
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long solve(long long n) {
+	long long count = 0;
+	vector<bool> prime;
+	for (int i = 2; i <= 1000000; i++)
+		prime.push_back(true);
+	for (int i = 2; i <= 1000000; i++){
+		if (prime[i]){
+			count++;
+			if (count == n)
+				return i; 
+			for (int j = 2*i; j <= 1000000; j += i)
+				prime[j] = false;
+		}
+	}
+}
+
+int main() {
+	cout << "Result: " << solve(10001);
+	return 0;
+}
