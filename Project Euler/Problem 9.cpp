bdaf0869008ca99b3e2c14d772b9a93b383209ec
ddk992001 <63284880+ddk992001@users.noreply.github.com>
2021-05-27 16:02:47 +0700

    add p9

---
+++ Project Euler/Problem 9.cpp
@@ -0,0 +1,22 @@
+// 25 - 1 - 2002
+
+#include <iostream>
+
+using namespace std;
+
+int solve() {
+	for (int i = 1; i < 500; i++){
+		for (int j = 1; j < 500; j++){
+			for (int k = 1; k < 500; k++){
+				if (i + j + k == 1000 && i*i + j*j == k*k)
+					return i*j*k;
+			}
+		}
+	}
+}
+
+int main() {
+	cout << "Result: " << solve();
+	return 0;
+}
+
