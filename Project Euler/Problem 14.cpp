7c570e2752c82534fac23e9daef55cd75f2f6192
ddk992001 <63284880+ddk992001@users.noreply.github.com>
2021-06-03 16:01:44 +0700

    add p14

---
+++ Project Euler/Problem 14.cpp
@@ -0,0 +1,46 @@
+// 5 - 4 - 2002
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int countTerm(long long n, vector<bool>& flag){
+	int count = 1;
+	long long temp = n;
+	flag[n] = false;
+	while (n != 1){
+		if (n % 2 == 0)
+			n /= 2;
+		else
+			n = 3*n + 1;
+		count++;
+		if (n < temp)
+			flag[n] = false;
+	}
+	flag[n] = false;
+	return count;
+}
+
+long long solve(long long n) {
+	vector<bool> flag;
+	int max = 0;
+	long long res = 0;
+	for (long long i = 0; i < n; i++)
+		flag.push_back(true);
+	for (long long i = 1; i < n; i++){
+		if (flag[i]){
+			int temp = countTerm(i, flag);
+			if (max < temp){
+				max = temp;	
+				res = i;
+			}
+		}
+	}	
+	return res;
+}
+
+int main() {
+	cout << "Result: " << solve(1000000);
+	return 0;
+}
