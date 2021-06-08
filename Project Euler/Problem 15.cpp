375dde55a09c0bc3e9cae3944fe59750a8c8c87a
ddk992001 <63284880+ddk992001@users.noreply.github.com>
2021-06-08 15:57:30 +0700

    add p15

---
+++ Project Euler/Problem 15.cpp
@@ -0,0 +1,20 @@
+// 19 - 4 - 2002
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long solve(long long n) {
+	long long temp = 1;
+	for (long long i = n+1; i <= 2*n; i++){
+		temp *= i;
+		temp /= (i - n);
+	}
+	return temp;
+}
+
+int main() {
+	cout << "Result: " << solve(20);
+	return 0;
+}
